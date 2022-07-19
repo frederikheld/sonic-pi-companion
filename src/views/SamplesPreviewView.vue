2465e59b5959368fe8b175e6267dd670056f474b
Frederik S. Held <git@frederikheld.de>
2022-07-20 00:37:10 +0200

    Direct access to sample types

--- src/views/SamplesPreviewView.vue
+++ src/views/SamplesPreviewView.vue
@@ -1,22 +1,26 @@
 <template>
-  <v-container>
-    <v-app-bar app>
-        <v-slide-group show-arrows>
-          <v-slide-item v-for="(samples, type) in samples" :key="type">
-            <v-btn :href="'#' + type" depressed rounded>{{ type }}</v-btn>
-          </v-slide-item>
-        </v-slide-group>
+  <v-container ref="container" id="container">
+    <v-app-bar app class="app-bar">
+      <v-slide-group v-model="activeSection" show-arrows>
+        <v-slide-item v-for="(samples, type) in samples" :key="type" v-slot="{ active, toggle }">
+          <v-btn :href="'#' + type" @click="toggle" :input-value="active" depressed rounded active-class="primary">{{ type }}</v-btn>
+        </v-slide-item>
+      </v-slide-group>
     </v-app-bar>
 
-    <!-- <v-row>
-      <v-col>
-        <pre style="height: 300px; overflow-y: scroll;">{{ $store.getters.samplesByType }}</pre>
-      </v-col>
-    </v-row> -->
-    <section v-for="(samples, type) in samples" :key="type" :id="type">
+    <!-- <div style="width: 120px; display: flex;">
+      <v-icon>mdi-speaker</v-icon>
+      <v-switch v-model="multiplePlayback" style="width: 42px;"></v-switch>
+      <v-switch prepend-icon="mdi-speaker" append-icon="mdi-speaker-multiple" v-model="multiplePlayback"></v-switch>
+      <v-icon>mdi-speaker-multiple</v-icon>
+    </div> -->
+
+    <!-- <div style="position: fixed; top: 72px; right: 16px; background: #fcc; z-index: 100;">{{ activeSection }}</div> -->
+
+    <section v-for="(samples, type) in samples" :key="type" :id="type" :ref="'sample_' + type">
       <v-row>
-        <v-col class="pa-3">
-          <h3 class="width: 100%;">{{ type }}</h3>
+        <v-col class="pa-3 mt-4 mb-0">
+          <span class="text-h5">{{ type }}</span>
         </v-col>
       </v-row>
       <v-row class="grid-row">
@@ -56,7 +60,10 @@ export default {
   components: { },
   data () {
     return {
-      currentlyPlaying: []
+      currentlyPlaying: [],
+      multiplePlayback: true,
+      activeSection: undefined,
+      sectionAnchors: []
     }
   },
   computed: {
@@ -64,23 +71,103 @@ export default {
       return this.$store.getters.samplesByType
     }
   },
+  mounted () {
+    this.sectionAnchors = this.getSectionAnchors()
+    this.setScrollEventListener(this.sectionAnchors)
+  },
   methods: {
     playSample (sample) {
       const audio = new Audio(require('@/' + sample.path))
       this.currentlyPlaying.push(sample.name)
       audio.play()
+
       audio.addEventListener('ended', (event) => {
         this.currentlyPlaying = this.currentlyPlaying.filter(item => item !== sample.name)
       })
     },
+    stopAllPlayback (sample) {
+
+    },
     isPlaying (sample) {
       return this.currentlyPlaying.includes(sample.name)
+    },
+    getSectionAnchors () {
+      const sections = []
+      Object.entries(this.$refs).forEach(([key, value]) => {
+        if (key.split('_')[0] === 'sample') {
+          sections.push(value[0])
+        }
+      })
+      return sections
+    },
+    setScrollEventListener (elements) {
+      document.addEventListener('scroll', () => {
+        function pageIsScrolledToBottom (margin) {
+          const scrollMaxY = Math.max(
+            document.body.scrollHeight,
+            document.body.offsetHeight,
+            document.documentElement.clientHeight,
+            document.documentElement.scrollHeight,
+            document.documentElement.offsetHeight
+          )
+          return scrollY > scrollMaxY - visualViewport.height - margin
+        }
+
+        const margin = 48
+        if (pageIsScrolledToBottom(margin)) {
+          this.activeSection = this.sectionAnchors.findIndex(item => item.id === elements.at(-1).getAttribute('id'))
+          return
+        }
+
+        elements.forEach(el => {
+          if (scrollY >= el.offsetTop - el.clientHeight / 6) {
+            // v-slide-item uses the index of the item for active state,
+            // so we have to find the index of that element's id in our array:
+            this.activeSection = this.sectionAnchors.findIndex(item => item.id === el.getAttribute('id'))
+          }
+        })
+      })
     }
   }
 }
 </script>
 
 <style scoped>
+
+#container {
+  scroll-behavior: smooth;
+}
+/* APP BAR */
+
+/* .app-bar /deep/ .v-toolbar__content {
+  padding-left: 0;
+  padding-right: 0;
+} */
+
+section {
+  display: block !important;
+}
+
+/* .app-bar {
+  display: inline-grid;
+  grid-template-columns: auto 1fr;
+  /* width: 100%; */
+  /* background: red; */
+/* } */
+
+/* .app-bar .quick-access-links { }
+
+.app-bar .playback-toggle {
+  align-content: right;
+} */
+
+/* .app-bar-container .playback-toggle > div {
+  width: 200px;
+  background-color: #422;
+} */
+
+/* DRUM PADS */
+
 .grid {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
@@ -145,7 +232,18 @@ section {
     /* color: #f33; */
   }
 
+.playback-toggle {
+  /* display: inline; */
+  /* white-space: nowrap; */
+}
+
+.playback-toggle > * {
+  /* display: inline; */
+  float: left;
+}
+
 /* CSS ANIMATION */
+
 .playing .playing-indicator {
   position: absolute;
   right: 0.9rem;
